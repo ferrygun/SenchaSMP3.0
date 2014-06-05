f28d45377a7a583408609c5cc54c57ea66d9c086
ferrygun <ferrygun@yahoo.com>
2014-06-05 23:57:43 +0800

    Update Note.js

--- app/controller/Note.js
+++ app/controller/Note.js
@@ -28,18 +28,19 @@ Ext.define("FD4.controller.Note",{
 	init: function () {
 		Ext.Viewport.setMasked({
 			xtype: 'loadmask',
-            message: 'Loading....'
+            message: 'Loading...'
         });
 
 		Ext.Ajax.request({
-			url: "https://smp-p1940517092trial.hanatrial.ondemand.com/com.FD.sales/",
+			url: "http://192.168.0.14/sapgw/com.FD.Demo/",
 			headers: {
 				"Content-Type" : "application/atom+xml; charset=UTF-8", 
+				"Authorization" : 'Basic UDE5NDA1MTcxMTY6UGFwYW1hbWExOA=='
 			},
 			method: 'GET',
 			withCredentials: true,
 			useDefaultXhrHeader: false,
-			disableCaching: true,
+			disableCaching: false,
 			success: function(response, opts){
 				console.log('GET Success'); 
 				Ext.Viewport.setMasked(false); 
@@ -49,31 +50,55 @@ Ext.define("FD4.controller.Note",{
 				console.log(response.statusText); 
 				if(response.statusText == 'Not Found' || response.statusText == 'Forbidden' ) {
 
+					if(Ext.os.is.iPad) {
+						devtype = 'iPad'
+					} else if(Ext.os.is.iPhone) {
+						devtype = 'iPhone'
+					} else if(Ext.os.is.iPod) {
+						devtype = 'iPod'
+					} else if(Ext.os.is.iOS) {
+						devtype = 'iOS'
+					} else if(Ext.os.is.MacOS) {
+						devtype = 'iOS'
+					} else if(Ext.os.is.Android) {
+						devtype = 'Android'
+					} else if(Ext.os.is.BlackBerry) {
+						devtype = 'BlackBerry'
+					} else if(Ext.os.is.Windows) {
+						devtype = 'Windows'
+					} else if(Ext.os.is.Other) {
+						devtype = 'Unknown'
+					}
+
+					//alert(devtype);
 					var regXML = '<?xml version="1.0" encoding="utf-8"?>' +
 					          '<entry xmlns="http://www.w3.org/2005/Atom" xmlns:m="http://schemas.microsoft.com/ado/2007/08/dataservices/metadata" xmlns:d="http://schemas.microsoft.com/ado/2007/08/dataservices">' +
 							  '<content type="application/xml">' +
 					          '<m:properties>' +
-					          '<d:DeviceType>iPhone</d:DeviceType>' + 
+					          '<d:DeviceType>' + devtype + '</d:DeviceType>' + 
 				              '</m:properties>' +
 							  '</content>' +
 							  '</entry>' 
 
 					Ext.Ajax.request({
-						url: 'https://smp-p1940517092trial.hanatrial.ondemand.com/odata/applications/latest/com.FD.sales/Connections',
+						url: 'http://192.168.0.14/sapgw/odata/applications/latest/com.FD.Demo/Connections',
 						headers: {
 							"Content-Type" : "application/atom+xml; charset=UTF-8", 
+							"Authorization" : 'Basic UDE5NDA1MTcxMTY6UGFwYW1hbWExOA=='
 						},
 						method: 'POST',
+						
 						withCredentials: true,
 						useDefaultXhrHeader: false,
 						xmlData: regXML,
-						disableCaching: true,
+						disableCaching: false,
 						success: function(response, opts){
 							console.log("HTTP POST"); 
 							Ext.Viewport.setMasked(false); 
 						},
 						failure : function(response, opts) { 
 							console.log("POST Failure"); 
+							console.log(response);
 							Ext.Viewport.setMasked(false); 
 						},
 					}); 
@@ -103,8 +128,15 @@ Ext.define("FD4.controller.Note",{
 						enablePagingParams: false,
 						withCredentials: true,	
 						username: 'P1940517092',
-						password: 'password',
-						url: "https://smp-p1940517092trial.hanatrial.ondemand.com/com.FD.sales/CountryCollection('" + formData.CountryCode + "')",
+						password: 'Initial123',
+						url: "http://192.168.0.14/sapgw/com.FD.Demo/CountryCollection('" + formData.CountryCode + "')",
+						listeners: {
+							'exception': function (proxy, response, operation) {
+								FD4.api.Error.alert(response);
+								console.log(response);
+
+							}
+						},
 					},
 
 
@@ -135,5 +167,4 @@ Ext.define("FD4.controller.Note",{
 	},
 
 	
-
 });
